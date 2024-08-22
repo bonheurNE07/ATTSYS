2244774f57c5d75baf710a607c4170e91b2a6f76
bonheurNE07 <bonheurndezenc@gmail.com>
2024-08-22 09:00:20 +0200

    create view for a new attendance record

--- attendance/views.py
+++ attendance/views.py
@@ -18,3 +18,14 @@ class AttendanceRecordDetailView(DetailView):
     model = AttendanceRecord
     template_name = 'attendance/attendance_record_detail.html'
     context_object_name = 'attendance_record'
+
+
+# Create view for a new attendance record
+class AttendanceRecordCreateView(CreateView):
+    model = AttendanceRecord
+    form_class = AttendanceRecordForm
+    template_name = 'attendance/attendance_record_form.html'
+    success_url = reverse_lazy('attendance:attendance_record_list')  # Redirect to the list after creation
+
+    def form_valid(self, form):
+        return super().form_valid(form)
